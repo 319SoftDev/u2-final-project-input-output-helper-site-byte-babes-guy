5c103f8bf3be7839089ce6e43856cc3bbe0d4f52
Sebantperez <165824241+Sebantperez@users.noreply.github.com>
2024-11-27 10:15:10 -0500

    subtle changes

--- BookClosed/book.js
+++ BookClosed/book.js
@@ -1,15 +1,8 @@
 // Get buttons and book elements
 const openButton = document.getElementById('open-book');
-const resetButton = document.getElementById('reset-book');
 const book = document.querySelector('.book');
-const pages = document.querySelectorAll('.page, .cover');
 
 // Function to open the book in the middle
 openButton.addEventListener('click', () => {
     book.classList.add('open'); // Apply the open class to the book
-});
-
-// Function to reset the book
-resetButton.addEventListener('click', () => {
-    book.classList.remove('open'); // Remove the open class to reset the book
 });
